7663d7590fc1749bd32aed3fa8967caedf6db759
David <davidmunyiri2@gmail.com>
2020-09-08 12:43:54 +0300

    Create serializers.py

---
+++ nairobi_hospitals_api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework_gis.serializers import GeoFeatureModelSerializer
+from rest_framework import serializers
+
+from . models import NairobiSubCounties, NairobiHealthFacilities
+
+class NairobiSubCountiesSerializer(GeoFeatureModelSerializer):
+
+    class Meta:
+        model = NairobiSubCounties
+        fields = '__all__'
+        geo_field = 'geom'
+
+
+class NairobiHealthFacilitiesSerializer(GeoFeatureModelSerializer):
+
+    distance = serializers.CharField()
+
+    class Meta:
+        model = NairobiHealthFacilities
+        fields = '__all__'
+        geo_field = 'geom'
+        read_only_fields = ['distance']
+
